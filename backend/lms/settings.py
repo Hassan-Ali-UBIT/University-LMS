a5255ab70fd634520c9b48ce4431a5ab6edc21ff
Hassan <hassanali4112@gmail.com>
2025-04-23 16:46:00 +0500

    added neon config

--- backend/lms/settings.py
+++ backend/lms/settings.py
@@ -106,6 +106,9 @@ DATABASES = {
         "PASSWORD": os.getenv("DB_PASSWORD"),
         "HOST": os.getenv("DB_HOST"),
         "PORT": os.getenv("DB_PORT"),
+        "OPTIONS": {
+            "sslmode": "require",  # Neon requires SSL
+        },
     }
 }
 
